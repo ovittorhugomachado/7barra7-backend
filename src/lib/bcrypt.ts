0bbf743a0e1ccb0202d1c7ebf88f7d7932d72120
Vitor Hugo <virtualpage.dev@gmail.com>
2025-11-27 11:16:48 -0300

    bcrypt configurado

---
+++ src/lib/bcrypt.ts
@@ -0,0 +1,12 @@
+import bcrypt from "bcrypt";
+
+type EncryptPassword = (password: string) => Promise<string>;
+type ComparePassword = (password: string, hash: string) => Promise<boolean>;
+
+export const encryptPassword: EncryptPassword = async (password) => {
+  return bcrypt.hash(password, 12);
+};
+
+export const comparePassword: ComparePassword = async (password, hash) => {
+  return bcrypt.compare(password, hash);
+};
